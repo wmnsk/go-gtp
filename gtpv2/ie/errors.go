117b379e1c4c5eee381e458503e3ce47356c9f8e
Yoshiyuki Kurauchi <ahochauwaaaaa@gmail.com>
2020-07-17 18:43:11 +0900

    v2/update IPAddress IE to use net.IP instead of string

--- gtpv2/ie/errors.go
+++ gtpv2/ie/errors.go
@@ -14,8 +14,9 @@ var (
 	ErrTooShortToParse = errors.New("too short to decode as GTP")
 	ErrInvalidLength   = errors.New("length value is invalid")
 
-	ErrInvalidType = errors.New("invalid type")
-	ErrIENotFound  = errors.New("could not find the specified IE in a grouped IE")
+	ErrInvalidType     = errors.New("invalid type")
+	ErrIENotFound      = errors.New("could not find the specified IE in a grouped IE")
+	ErrIEValueNotFound = errors.New("could not find the specified value in an IE")
 
 	ErrMalformed = errors.New("malformed IE")
 )
