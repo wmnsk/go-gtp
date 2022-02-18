4fe00b3c6b266a6f9f562a9f02924992b151fd9d
Bengt Kleberg <bengt.kleberg@travelping.com>
2022-02-18 09:24:13 +0100

    Add support for suspend/resume notification/ack

---
+++ gtpv2/message/resume-notification.go
@@ -0,0 +1,184 @@
+// Copyright 2019-2021 go-gtp authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package message
+
+import (
+	"github.com/wmnsk/go-gtp/gtpv2/ie"
+)
+
+// ResumeNotification is a ResumeNotification Header and its IEs.
+type ResumeNotification struct {
+	*Header
+	IMSI                       *ie.IE
+	LinkedEPSBearerID          *ie.IE
+	OriginatingNode            *ie.IE
+	SenderFTEIDForControlPlane *ie.IE
+	PrivateExtension           *ie.IE
+}
+
+// NewResumeNotification creates a new ResumeNotification.
+func NewResumeNotification(teid, seq uint32, ies ...*ie.IE) *ResumeNotification {
+	c := &ResumeNotification{
+		Header: NewHeader(
+			NewHeaderFlags(2, 0, 1),
+			MsgTypeResumeNotification, teid, seq, nil,
+		),
+	}
+
+	for _, i := range ies {
+		if i == nil {
+			continue
+		}
+		switch i.Type {
+		case ie.IMSI:
+			c.IMSI = i
+		case ie.EPSBearerID:
+			c.LinkedEPSBearerID = i
+		case ie.NodeType:
+			c.OriginatingNode = i
+		case ie.FullyQualifiedTEID:
+			c.SenderFTEIDForControlPlane = i
+		case ie.PrivateExtension:
+			c.PrivateExtension = i
+		}
+	}
+
+	c.SetLength()
+	return c
+}
+
+// Marshal serializes ResumeNotification into bytes.
+func (c *ResumeNotification) Marshal() ([]byte, error) {
+	b := make([]byte, c.MarshalLen())
+	if err := c.MarshalTo(b); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// MarshalTo serializes ResumeNotification into bytes.
+func (c *ResumeNotification) MarshalTo(b []byte) error {
+	if c.Header.Payload != nil {
+		c.Header.Payload = nil
+	}
+	c.Header.Payload = make([]byte, c.MarshalLen()-c.Header.MarshalLen())
+
+	offset := 0
+	if ie := c.IMSI; ie != nil {
+		if err := ie.MarshalTo(c.Payload[offset:]); err != nil {
+			return err
+		}
+		offset += ie.MarshalLen()
+	}
+	if ie := c.LinkedEPSBearerID; ie != nil {
+		if err := ie.MarshalTo(c.Payload[offset:]); err != nil {
+			return err
+		}
+		offset += ie.MarshalLen()
+	}
+	if ie := c.OriginatingNode; ie != nil {
+		if err := ie.MarshalTo(c.Payload[offset:]); err != nil {
+			return err
+		}
+		offset += ie.MarshalLen()
+	}
+	if ie := c.SenderFTEIDForControlPlane; ie != nil {
+		if err := ie.MarshalTo(c.Payload[offset:]); err != nil {
+			return err
+		}
+		offset += ie.MarshalLen()
+	}
+	if ie := c.PrivateExtension; ie != nil {
+		if err := ie.MarshalTo(c.Payload[offset:]); err != nil {
+			return err
+		}
+		offset += ie.MarshalLen()
+	}
+
+	c.Header.SetLength()
+	return c.Header.MarshalTo(b)
+}
+
+// ParseResumeNotification decodes given bytes as ResumeNotification.
+func ParseResumeNotification(b []byte) (*ResumeNotification, error) {
+	c := &ResumeNotification{}
+	if err := c.UnmarshalBinary(b); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+// UnmarshalBinary decodes given bytes as ResumeNotification.
+func (c *ResumeNotification) UnmarshalBinary(b []byte) error {
+	var err error
+	c.Header, err = ParseHeader(b)
+	if err != nil {
+		return err
+	}
+	if len(c.Header.Payload) < 2 {
+		return nil
+	}
+
+	decodedIEs, err := ie.ParseMultiIEs(c.Header.Payload)
+	if err != nil {
+		return err
+	}
+
+	for _, i := range decodedIEs {
+		if i == nil {
+			continue
+		}
+		switch i.Type {
+		case ie.IMSI:
+			c.IMSI = i
+		case ie.EPSBearerID:
+			c.LinkedEPSBearerID = i
+		case ie.NodeType:
+			c.OriginatingNode = i
+		case ie.FullyQualifiedTEID:
+			c.SenderFTEIDForControlPlane = i
+		case ie.PrivateExtension:
+			c.PrivateExtension = i
+		}
+	}
+
+	return nil
+}
+
+// MarshalLen returns the serial length in int.
+func (c *ResumeNotification) MarshalLen() int {
+	l := c.Header.MarshalLen() - len(c.Header.Payload)
+	if ie := c.IMSI; ie != nil {
+		l += ie.MarshalLen()
+	}
+	if ie := c.LinkedEPSBearerID; ie != nil {
+		l += ie.MarshalLen()
+	}
+	if ie := c.OriginatingNode; ie != nil {
+		l += ie.MarshalLen()
+	}
+	if ie := c.SenderFTEIDForControlPlane; ie != nil {
+		l += ie.MarshalLen()
+	}
+	if ie := c.PrivateExtension; ie != nil {
+		l += ie.MarshalLen()
+	}
+	return l
+}
+
+// SetLength sets the length in Length field.
+func (c *ResumeNotification) SetLength() {
+	c.Header.Length = uint16(c.MarshalLen() - 4)
+}
+
+// MessageTypeName returns the name of protocol.
+func (c *ResumeNotification) MessageTypeName() string {
+	return "Resume Notification"
+}
+
+// TEID returns the TEID in uint32.
+func (c *ResumeNotification) TEID() uint32 {
+	return c.Header.teid()
+}
---
+++ gtpv2/message/suspend-notification_test.go
@@ -0,0 +1,86 @@
+// Copyright 2019-2021 go-gtp authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package message_test
+
+import (
+	"testing"
+
+	"github.com/wmnsk/go-gtp/gtpv2"
+	"github.com/wmnsk/go-gtp/gtpv2/ie"
+	"github.com/wmnsk/go-gtp/gtpv2/message"
+	"github.com/wmnsk/go-gtp/gtpv2/testutils"
+)
+
+func TestSuspendNotification(t *testing.T) {
+	cases := []testutils.TestCase{
+		{
+			Description: "Normal",
+			Structured: message.NewSuspendNotification(
+				testutils.TestBearerInfo.TEID, testutils.TestBearerInfo.Seq,
+				ie.NewIMSI("123451234567890"),
+				ie.NewUserLocationInformationStruct(
+					nil, nil, nil, ie.NewTAI("123", "45", 0x0001),
+					ie.NewECGI("123", "45", 0x00000101), nil, nil, nil,
+				),
+				ie.NewEPSBearerID(5),
+				ie.NewPacketTMSI(0xdeadbeef),
+				ie.NewNodeType(gtpv2.NodeTypeMME),
+				ie.NewIPAddress("1.1.1.1"),
+				ie.NewPortNumber(2123),
+				ie.NewHopCounter(1),
+				ie.NewFullyQualifiedTEID(gtpv2.IFTypeS10MMEGTPC, 0xffffffff, "1.1.1.1", ""),
+				ie.NewPrivateExtension(123, []byte{1, 2, 3, 4}),
+			),
+			Serialized: []byte{
+				// Header
+				0x48, 162, 0x00, 97, 0x11, 0x22, 0x33, 0x44, 0x00, 0x00, 0x01, 0x00,
+				// IMSI
+				0x01, 0x00, 0x08, 0x00, 0x21, 0x43, 0x15, 0x32, 0x54, 0x76, 0x98, 0xf0,
+				// UserLocationInformation
+				0x56, 0x00, 0x0d, 0x00, 0x18,
+				0x21, 0xf3, 0x54, 0x00, 0x01,
+				0x21, 0xf3, 0x54, 0x00, 0x00, 0x01, 0x01,
+				// EPSBearerID
+				0x49, 0x00, 0x01, 0x00, 0x05,
+				// PacketTMSI
+				0x6f, 0x00, 0x04, 0x00, 0xde, 0xad, 0xbe, 0xef,
+				// NodeType
+				0x87, 0x00, 0x01, 0x00, 0x01,
+				// IPAddress
+				0x4a, 0x00, 0x04, 0x00, 0x01, 0x01, 0x01, 0x01,
+				// PortNumber
+				0x7e, 0x00, 0x02, 0x00, 0x08, 0x4b,
+				// HopCounter
+				0x71, 0x00, 0x01, 0x00, 0x01,
+				// FullyQualifiedTEID
+				0x57, 0x00, 0x09, 0x00, 0x8c, 0xff, 0xff, 0xff, 0xff, 0x01, 0x01, 0x01, 0x01,
+				// PrivateExtension
+				0xff, 0x00, 0x06, 0x00, 0x00, 123, 1, 2, 3, 4,
+			},
+		},
+		{
+			Description: "Erlang binary",
+			Structured: message.NewSuspendNotification(
+				1024, 127,
+				ie.NewIPAddress("1.2.3.4"),
+				ie.NewPortNumber(2345),
+			),
+			Serialized: []byte{
+				0x48, 0xa2, 0x00, 0x16, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00,
+				0x7f, 0x00, 0x4a, 0x00, 0x04, 0x00, 0x01, 0x02, 0x03, 0x04,
+				0x7e, 0x00, 0x02, 0x00, 0x09, 0x29,
+			},
+		},
+	}
+
+	testutils.Run(t, cases, func(b []byte) (testutils.Serializable, error) {
+		v, err := message.ParseSuspendNotification(b)
+		if err != nil {
+			return nil, err
+		}
+		v.Payload = nil
+		return v, nil
+	})
+}
