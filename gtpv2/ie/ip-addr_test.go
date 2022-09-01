74f753c9fbb9489aa34f72b327cf58c998b82b02
Egor Pats <egor.pats@gmail.com>
2022-09-01 15:42:46 +0300

    Fix tests

--- gtpv2/ie/ip-addr_test.go
+++ gtpv2/ie/ip-addr_test.go
@@ -6,90 +6,77 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/wmnsk/go-gtp/gtpv2"
 	"github.com/wmnsk/go-gtp/gtpv2/ie"
 )
 
 func TestPDNAddressAllocationIP(t *testing.T) {
-
-	type IPAddr struct {
-		ip  net.IP
-		err error
-	}
 	cases := []struct {
 		description string
 		paa         *ie.IE
-		ipv4        IPAddr
-		ipv6        IPAddr
+		pdnType     uint8
+		ipv4        net.IP
+		ipv6        net.IP
 	}{
 		{
 			"PDNType IPv4",
 			ie.NewPDNAddressAllocation("1.2.3.4"),
-			IPAddr{
-				net.ParseIP("1.2.3.4"),
-				nil,
-			},
-			IPAddr{
-				nil,
-				ie.ErrIEValueNotFound,
-			},
+			gtpv2.PDNTypeIPv4,
+			net.ParseIP("1.2.3.4"),
+			nil,
 		},
 		{
 			"PDNType IPv6",
 			ie.NewPDNAddressAllocation("::1"),
-			IPAddr{
-				nil,
-				ie.ErrIEValueNotFound,
-			},
-			IPAddr{
-				net.ParseIP("::1"),
-				nil,
-			},
+			gtpv2.PDNTypeIPv6,
+			nil,
+			net.ParseIP("::1"),
 		},
 		{
 			"PDNType IPv4v6",
-			ie.NewPDNAddressAllocationDual("1.2.3.4", "::1", uint8(64)),
-			IPAddr{
-				net.ParseIP("1.2.3.4"),
-				nil,
-			},
-			IPAddr{
-				net.ParseIP("::1"),
-				nil,
-			},
+			ie.NewPDNAddressAllocationDual("1.2.3.4", "::1", 64),
+			gtpv2.PDNTypeIPv4v6,
+			net.ParseIP("1.2.3.4"),
+			net.ParseIP("::1"),
 		},
 		{
 			"PDNType NonIP",
 			ie.NewPDNAddressAllocation(""),
-			IPAddr{
-				nil,
-				io.ErrUnexpectedEOF,
-			},
-			IPAddr{
-				nil,
-				io.ErrUnexpectedEOF,
-			},
+			gtpv2.PDNTypeNonIP,
+			nil,
+			nil,
 		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.description, func(t *testing.T) {
-
-			ipv4, err := c.paa.IPv4()
-			if diff := cmp.Diff(ipv4, c.ipv4.ip); diff != "" {
+			pdnType := c.paa.MustPDNType()
+			if diff := cmp.Diff(pdnType, c.pdnType); diff != "" {
 				t.Error(diff)
 			}
-			if err != c.ipv4.err {
-				t.Errorf("IPv4() error = %v, want %v", err, c.ipv4.err)
-			}
 
-			ipv6, err := c.paa.IPv6()
-			if diff := cmp.Diff(ipv6, c.ipv6.ip); diff != "" {
+			ipv4, _ := c.paa.IPv4()
+			if diff := cmp.Diff(ipv4, c.ipv4); diff != "" {
 				t.Error(diff)
 			}
-			if err != c.ipv6.err {
-				t.Errorf("IPv6() error = %v, want %v", err, c.ipv6.err)
+
+			ipv6, _ := c.paa.IPv6()
+			if diff := cmp.Diff(ipv6, c.ipv6); diff != "" {
+				t.Error(diff)
 			}
 
+			ip, err := c.paa.IP()
+			if err == nil {
+				v := ipv4
+				if pdnType == gtpv2.PDNTypeIPv6 {
+					v = ipv6
+				}
+				if diff := cmp.Diff(ip, v); diff != "" {
+					t.Error(diff)
+				}
+			} else if err != io.ErrUnexpectedEOF {
+				t.Error(err)
+			}
 		})
 	}
 }
